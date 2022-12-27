84982fce31058332982410599fe78805df277f99
Nico Weilmünster <nico.weili@web.de>
2022-12-27 18:02:04 +0100

    winver

--- pc/main.py
+++ pc/main.py
@@ -1,5 +1,6 @@
 import socketio
 import subprocess
+import platform
 
 
 # asyncio
@@ -25,9 +26,13 @@ def message(data):
         print("starte zoom")
         subprocess.call([r'C:\Users\nicow\AppData\Roaming\Zoom\bin\Zoom.exe'])
     elif data == "teams":
-        print("starte teams")
-        subprocess.call([r'msteams'])
-        
+        print("starte teams")     
+        version = int(platform.version().split('.')[2])
+        if(version >= 22621):
+            subprocess.call(r'msteams')
+        else:
+            subprocess.call([r'C:\Users\nicow\AppData\Local\Microsoft\Teams\current\Teams.exe'])
+
 if __name__ == '__main__':
     sio.connect('http://192.168.0.180:5000')
 
