b94b6b4f2b027c71e9651bf7875b62310f02bc77
Nico Weilmünster <nico.weili@web.de>
2022-12-27 17:35:04 +0100

    pc steuerung

--- pc/main.py
+++ pc/main.py
@@ -1,1 +1,33 @@
-print("hello")
+import socketio
+import subprocess
+
+
+# asyncio
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("I'm connected!")
+
+@sio.event
+def connect_error(data):
+    print("The connection failed!")
+
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
+
+
+@sio.event
+def message(data):
+    print('I received a message!', data)
+    if data == "zoom":
+        print("starte zoom")
+        subprocess.call([r'C:\Users\nicow\AppData\Roaming\Zoom\bin\Zoom.exe'])
+    elif data == "teams":
+        print("starte teams")
+        subprocess.call([r'msteams'])
+        
+if __name__ == '__main__':
+    sio.connect('http://192.168.0.180:5000')
+
